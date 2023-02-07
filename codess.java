89fbab9da89cfd6df07227673426ff4b9c05fc03
Palak Aggarwal <100852067+aipalak@users.noreply.github.com>
2023-02-07 23:08:37 +0530

    Add files via upload

---
+++ codess.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class codess {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int m=scn.nextInt();
+        int[][] arr=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                arr[i][j]=scn.nextInt();
+            }
+        }
+        setZeros(arr);
+        display(arr);
+    }
+    public static void setZeros(int matrix[][]) {
+        // Write your code here..
+        int row=0;
+        int col=0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    row=i;
+                    col=j;
+                }
+            }
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(i==row || j==col){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+    public static void display(int[][] a ){
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a[0].length;j++){
+                System.out.print(a[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
